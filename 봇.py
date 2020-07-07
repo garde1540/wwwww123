68ca262f42d55a9179f0f56b5daaf4c47d5475e2
garde1540 <67937699+garde1540@users.noreply.github.com>
2020-07-07 20:36:37 +0900

    Add files via upload

---
+++ 봇.py
@@ -0,0 +1,29 @@
+import discord
+import os
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("날누르고 메시지 #할말")
+    await client.change_presence(status=discord.Status.online, activity=game)
+      
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("#"):
+        msg = message.content[1:]
+        await client .get_channel(int(730010475831623682)).send(msg)
+
+
+
+
+
+
+
+
+access_token = os.environ["BOT_TOKEN"]
+client.run("NzI5OTk2MzcwNTQ2NTI0MjUx.XwRc2g.1kR9TlVU7RKRYHZXLhYEIrSILX0")
